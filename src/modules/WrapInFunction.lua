12bea19af644a4912837f69963a981136ea63368
vxvV4 <hoqxpxsqphwojohqcw@gmail.com>
2025-10-27 02:15:14 +0800

    Update WrapInFunction.lua

--- src/modules/WrapInFunction.lua
+++ src/modules/WrapInFunction.lua
@@ -1,6 +1,5 @@
 local Wrapper = {}
 function Wrapper.process(code)
-    return [[return(function(...)local a,b,c=1,{...},function(...)]]..code..[[end;return c((table.unpack or unpack)(b))end)(...)]]
+return [[return (function(...) ]]..code..[[ end)(...)]]
 end
-
 return Wrapper

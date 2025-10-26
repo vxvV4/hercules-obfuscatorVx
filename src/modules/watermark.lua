6998d83a02e2ea753b4581c76ea2fc115bc77a3b
vxvV4 <hoqxpxsqphwojohqcw@gmail.com>
2025-10-27 01:33:24 +0800

    Update watermark.lua

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 | hercules-obfuscator.xyz/discord | hercules-obfuscator.xyz/source]\n" .. code
+    return "--[ Obfuscated by Nation Alpha v1.6.2 ]\n" .. code
 end
 
 return Watermark
